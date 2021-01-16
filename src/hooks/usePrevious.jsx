69d58ba1c7a12a2c606e17a084c1e5c289706f35
Wang Haoran(Robin) <notrealrobin@gmail.com>
2021-01-16 13:59:32 -0800

    enter pin ui

---
+++ src/hooks/usePrevious.jsx
@@ -0,0 +1,15 @@
+import { useRef, useEffect } from "react";
+
+function usePrevious(value) {
+  const ref = useRef;
+
+  // Store current value in ref
+  useEffect(() => {
+    ref.current = value;
+  }, [value]); // Only re-run if value changes
+
+  // Return previous value (happens before update in useEffect above)
+  return ref.current;
+}
+
+export default usePrevious;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,76 @@
 import { Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
-import { useEffect } from "react";
+import { Button, ThemeProvider } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import OTPInput from "../custom-components/OTPInput";
+import "./Home.css";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const Home = () => {
   useEffect(() => {
     console.log("In home page");
   });
 
+  const [numOfPins, setNumOfPins] = useState(6);
+  const history = useHistory();
+
+  const theme = createMuiTheme({
+    darkMode: true,
+    palette: {
+      primary: {
+        light: "#1ed760",
+        main: "#1db954",
+        dark: "#1db954",
+        contrastText: "#fff",
+      },
+      secondary: {
+        light: "#ffffff",
+        main: "#000000",
+        dark: "#000000",
+        contrastText: "#000",
+      },
+    },
+    typography: {
+      button: {
+        textTransform: "none",
+        fontSize: "25px",
+      },
+    },
+  });
+
+  const handlePinInputChange = (otp) => {
+    if (otp.length === 6) {
+      console.log("finished");
+      history.push("/room");
+    }
+  };
+
   return (
-    <Link to="/room">
-      <Button variant="outlined">Join room</Button>
-    </Link>
+    <ThemeProvider theme={theme}>
+      <div id="HomePage">
+        <div id="PinBox">
+          <h1>Enter pin to join a room</h1>
+          <OTPInput
+            autoFocus
+            isNumberInput
+            length={numOfPins}
+            className="otpContainer"
+            inputClassName="otpInput"
+            onChangeOTP={(otp) => {
+              handlePinInputChange(otp);
+            }}
+          ></OTPInput>
+        </div>
+        <div className="ButtonContainer">
+          <Link to="/room" style={{ textDecoration: "none" }}>
+            <Button variant="contained" color="primary" className="button">
+              Start a your own room
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 };
 
