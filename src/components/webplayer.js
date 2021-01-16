de6444215553e5f6a48552169f3bcc37b547dc5e
tybens <tb19@princeton.edu>
2021-01-16 14:11:47 -0800

    a teensy bit better ui for pseudo-queue

--- src/components/webplayer.js
+++ src/components/webplayer.js
@@ -1,10 +1,8 @@
 const WebPlayer = ({ songUri }) => {
-  songUri = '2Lt2OCMsNF0DefG5H1NOqc';
-
   return (
     <iframe
       src={`https://open.spotify.com/embed/track/${songUri}`}
-      width="300"
+      width="360"
       height="80"
       frameborder="0"
       allowtransparency="true"
--- src/pages/[roomId]/index.js
+++ src/pages/[roomId]/index.js
@@ -1,13 +1,16 @@
 import { Link, useLocation } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import { useEffect, useState } from 'react';
-import WebPlayer from '../../components/webplayer';
 import Grid from '@material-ui/core/Grid';
+import WebPlayer from '../../components/webplayer';
+import QueueList from '../../components/queue-list';
 
 const Room = () => {
   let location = useLocation();
   var [roomId, setRoomId] = useState(null);
   var [currentSongUri, setCurrentSongUri] = useState('2Lt2OCMsNF0DefG5H1NOqc');
+
+  // TODO: setCurrentSongUri from search bar
 
   // after "page" component loads, check location
   useEffect(() => {
@@ -24,6 +27,9 @@ const Room = () => {
       <Grid item>
         <WebPlayer songUri={currentSongUri} />
       </Grid>
+
+      <QueueList roomId={roomId} />
+
       <Grid item>
         <Link to="/">
           <Button variant="contained" color="secondary" size="large">
