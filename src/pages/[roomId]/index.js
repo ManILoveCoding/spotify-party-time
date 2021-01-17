edd113d2f5dc0490a5c5d6a1a9430f982f5e1599
tybens <tb19@princeton.edu>
2021-01-17 03:21:31 -0800

    room id url parsing for access code

--- src/pages/[roomId]/index.js
+++ src/pages/[roomId]/index.js
@@ -5,17 +5,24 @@ import Grid from '@material-ui/core/Grid';
 import WebPlayer from '../../components/webplayer';
 import QueueList from '../../components/queue-list';
 import SearchBar from '../../components/searchbar';
+import getAccessCode from '../CreateRoom';
 
 const Room = () => {
   const inputRef = useRef();
   let location = useLocation();
   var [roomId, setRoomId] = useState(null);
+  var [access_code, setAccessCode] = useState('');
   var [currentSong, setCurrentSong] = useState('2Lt2OCMsNF0DefG5H1NOqc');
+
+  // TODO: get access_token from robin stuff
+  var access_token =
+    'BQDuvYrH_s7VaTxroMaI7fXlXgwLet9DdJwdm2DohmFIB9Nzvoj8-jsCBKZ2-D6O4v4YD8Fsey0VQO9BVDiOUxj81lSw4TM8RlUqZy8chbHqK27HGQzMhMu5xfS-T4tciB3eW1ZAGw';
 
   // after "page" component loads, check location
   useEffect(() => {
     if (location) {
-      setRoomId(location.pathname.split('/')[2]);
+      setRoomId(location.pathname.split('/')[2].split('?')[0]);
+      setAccessCode(location.pathname.split('/')[2].split('?')[1]);
     }
   }, [location]);
 
@@ -26,6 +33,7 @@ const Room = () => {
       </Grid>
       <Grid item>
         <SearchBar
+          access_token={access_token}
           style={{ height: '40px', width: '350px', fontSize: '15px' }}
           ref={inputRef}
           onSelect={(song) => {
@@ -35,7 +43,7 @@ const Room = () => {
         />
       </Grid>
       <Grid item>
-        <WebPlayer songId={currentSong.id} />
+        <WebPlayer access_token={access_token} songId={currentSong.id} />
       </Grid>
 
       <QueueList roomId={roomId} />
